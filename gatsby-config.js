5350d5cb397a61e983eeeacceb980bf548fd4bab
Hugo Marques <hugomarquesdev@gmail.com>
2021-12-17 18:07:36 +0000

    video update + gitlab 1st try

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,103 +1,104 @@
 const myCustomQueries = {
-  mobile: '(max-width: 768px)',
+    mobile: '(max-width: 768px)',
 };
 
 module.exports = {
-  siteMetadata: {
-    title: `Insidepipe`,
-    description: `Insidepipe`,
-    author: `@invisual`,
-    siteUrl: 'https://www.insidepipe.pt',
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    pathPrefix: '/insidepipe',
+    siteMetadata: {
+        title: `Insidepipe`,
+        description: `Insidepipe`,
+        author: `@invisual`,
+        siteUrl: 'https://www.insidepipe.pt',
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `data`,
-        path: `${__dirname}/src/data`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `blog`,
-        path: `${__dirname}/src/blog`,
-      },
-    },
-    `gatsby-transformer-json`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-sass`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        start_url: `/`,
-        icon: `src/images/logo-w.svg`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-image`,
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 4000,
-              linkImagesToOriginal: false,
+                name: `images`,
+                path: `${__dirname}/src/images`,
             },
-          },
-        ],
-      },
-    },
-    {
-      resolve: "gatsby-plugin-breakpoints",
-      options: {
-          queries: myCustomQueries,
-      },
-  },
-    {
-      resolve: 'gatsby-plugin-mailchimp',
-      options: {
-          endpoint: 'https://invisual.us6.list-manage.com/subscribe/post?u=2f18b89cb2f16b1058db20e3b&amp;id=9d520d3e40', // string; add your MC list endpoint here; see instructions below
-          timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        trackingIds: ["UA-201768842-1"],
-        gtagConfig: {
-          anonymize_ip: true,
-          cookie_expires: 0,
         },
-        pluginConfig: {
-          head: true,
-          respectDNT: true,
-          exclude: ["/404/**"],
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `data`,
+                path: `${__dirname}/src/data`,
+            },
         },
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-htaccess',
-      options: {
-        DisallowSymLinks: true,
-      },
-    },
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-robots-txt`,
-    `gatsby-plugin-offline`,
-  ],
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `blog`,
+                path: `${__dirname}/src/blog`,
+            },
+        },
+        `gatsby-transformer-json`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        `gatsby-plugin-styled-components`,
+        `gatsby-plugin-sass`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                start_url: `/`,
+                icon: `src/images/logo-w.svg`, // This path is relative to the root of the site.
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            // It's important to specify the maxWidth (in pixels) of
+                            // the content container as this plugin uses this as the
+                            // base for generating different widths of each image.
+                            maxWidth: 4000,
+                            linkImagesToOriginal: false,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            resolve: "gatsby-plugin-breakpoints",
+            options: {
+                queries: myCustomQueries,
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-mailchimp',
+            options: {
+                endpoint: 'https://invisual.us6.list-manage.com/subscribe/post?u=2f18b89cb2f16b1058db20e3b&amp;id=9d520d3e40', // string; add your MC list endpoint here; see instructions below
+                timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+                trackingIds: ["UA-201768842-1"],
+                gtagConfig: {
+                    anonymize_ip: true,
+                    cookie_expires: 0,
+                },
+                pluginConfig: {
+                    head: true,
+                    respectDNT: true,
+                    exclude: ["/404/**"],
+                },
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-htaccess',
+            options: {
+                DisallowSymLinks: true,
+            },
+        },
+        `gatsby-plugin-sitemap`,
+        `gatsby-plugin-robots-txt`,
+        `gatsby-plugin-offline`,
+    ],
 }
